624527497318342705a8b6a5b8334cc7d2a07411
Monir Hossain <monir198@gmail.com>
2026-05-06 09:13:14 +0600

    ProductCreatedEvent moved to seperate project and added here as dependency so that both producer and consumer  can interact

--- src/main/java/com/example/mhb/ProductsMicroservice/services/ProductServiceImpl.java
+++ src/main/java/com/example/mhb/ProductsMicroservice/services/ProductServiceImpl.java
@@ -4,6 +4,7 @@ import java.util.UUID;
 import java.util.concurrent.CompletableFuture;
 
 import com.example.mhb.ProductsMicroservice.model.CreateProductRestModel;
+import com.example.mhb.core.ProductCreatedEvent;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
